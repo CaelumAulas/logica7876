ae1b32c47767b07cf129394da5fd60a0aeb9ad59
Ana Losnak <ana.losnak@caelum.com.br>
2018-12-08 12:17:24 -0200

    Usando função com parâmetro

--- libRelogio.js
+++ libRelogio.js
@@ -1,6 +1,5 @@
-var tempo
-function formataRelogio(){
-    var tempoEmHorasFloat = tempo/60
+function formataRelogio(tempoMinutos){
+    var tempoEmHorasFloat = tempoMinutos/60
     var horas = parseInt(tempoEmHorasFloat)
     var minutos = parseInt((tempoEmHorasFloat - horas) * 60)
     return horas.toString().padStart(2,0) + "h" + minutos.toString().padStart(2,0) + "min"
